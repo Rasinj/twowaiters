befbb0cad48e249f1ffe3c618a93b01a665a667b
olle nordesjo <bob@example.com>
2021-01-31 12:46:32 +0000

    update readme and make sure the twowaiters is true to the description

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# twowaiters
+# Two Waiters
 A small example of two threads executing actions in python
+
+# What can I learn from this repository
+
+* Subclasssing (a subclassed thread that starts on initialisation)
+* Minimally using locks, queues and threads
+* Clear variable names
+* Which function called another function?
+* Longpolling
+* Light typing of function arguments
+* Comments only where necessary (here in if __name__ == "__main__")
+* Dataclass objects
+
+# How does it work?
+
+* An action queue with "tasks" is created
+* Two running threads running either "seldom" or "often" are associated
+* Each thread has a target that determines how to perform the task
+* A third "longpoll" thread polls for whether there are threads remaining
+
+# Extras
+ 
+A nice concurrency diagram from pycharm (yay!)
+
+![plot](concurrency_diagram_pycharm.png)
--- twowaiters.py
+++ twowaiters.py
@@ -13,6 +13,8 @@ from queue import Queue
 from dataclasses import dataclass
 from typing import List
 
+RESET_VALUE = -180
+
 
 @dataclass()
 class ValueDuration:
@@ -35,6 +37,11 @@ lock = threading.Lock()
 
 
 def perform_actions(actions: List[ValueDuration], **kwargs):
+    """
+    Perform each action using a lock.
+    Set a global (note that you may not want to do this in a production setting)
+    Also print the thread calling the function and the parent caller
+    """
     global current_value
     caller = inspect.stack()[1].function
     current_thread_name = threading.current_thread().name
@@ -48,13 +55,9 @@ def perform_actions(actions: List[ValueDuration], **kwargs):
         print(f"{caller} releasing lock\n")
 
 
-def run_seldom(actions_queue: Queue, wait_until_acquire_next_task=1):
+def run_seldom(actions_queue: Queue, wait_until_acquire_next_task: float = 1):
     """
     Threading target for one run mechanism that runs seldom
-
-    :param actions_queue:
-    :param wait_until_acquire_next_task:
-    :return:
     """
     while not actions_queue.empty():
         value_actions = actions_queue.get()
@@ -62,13 +65,9 @@ def run_seldom(actions_queue: Queue, wait_until_acquire_next_task=1):
         time.sleep(wait_until_acquire_next_task)
 
 
-def run_often(actions_queue: Queue, wait_until_acquire_next_task=0.2):
+def run_often(actions_queue: Queue, wait_until_acquire_next_task: float = 0.2):
     """
     Threading target for another run mechanism that runs often
-
-    :param actions_queue:
-    :param wait_until_acquire_next_task:
-    :return:
     """
     while not actions_queue.empty():
         value_actions = actions_queue.get()
@@ -86,42 +85,47 @@ def at_least_one_remaining_runner():
 
 
 def longpoll():
-    """ Periodically print the remaining actions"""
+    """ Periodically print the remaining actions """
     global current_value
     while at_least_one_remaining_runner():
         with lock:
             print("longpoll Acquired lock")
-            print(f"Current val: {current_value}\nRemaining : {actions_queue.queue}")
+            print(
+                f"Current val: {current_value}\nRemaining : {actions_queue.queue}")
             time.sleep(0.1)
             print("longpoll releasing lock\n")
         time.sleep(2)
 
 
 if __name__ == "__main__":
-    actions_queue = Queue()
-    reset_value = -180
+    # We first create a queue where each item will be a list of
+    # ValueDuration objects
 
     # Add some values into an actions queue.
-    # Each item is a list of ValueDuration objects
+    actions_queue = Queue()
     for lower_value in range(10, 50, 2):
         higher_value = lower_value + 5
         actions_queue.put(
             [
                 ValueDuration(lower_value, 0.1),
                 ValueDuration(higher_value, 0.2),
-                ValueDuration(reset_value, 0.1),
+                ValueDuration(RESET_VALUE, 0.1),
             ]
         )
 
-    # Make two runner threads
+    # Now we make two runner threads, one running "seldom" and one "often"
+    # Note that these will start upon initialisation
     runner_seldom = Thread(
-        target=run_seldom, kwargs={"actions_queue": actions_queue}, name="runner_seldom"
+        target=run_seldom, kwargs={"actions_queue": actions_queue},
+        name="runner_seldom"
     )
     runner_often = Thread(
-        target=run_often, kwargs={"actions_queue": actions_queue}, name="runner_often"
+        target=run_often, kwargs={"actions_queue": actions_queue},
+        name="runner_often"
     )
 
-    # Make another polling thread
+    # Make another polling thread to periodically print what
+    # remains in the actions_queue
     pollthread = Thread(target=longpoll, name="pollthread")
 
     # Wait for all threads to join
